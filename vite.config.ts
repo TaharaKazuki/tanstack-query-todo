6b7e87da6ac3012930d3180f60484c78de3d0d83
Tahara Kazuki <>
2022-12-21 00:12:18 +0900

    setting prettier

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true,
+    port: 8000,
+  },
   plugins: [react()],
 })
