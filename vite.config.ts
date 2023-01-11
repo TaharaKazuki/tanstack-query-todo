8c529684e7f18774abc58e5bb483f20a3bae78ad
Tahara Kazuki <>
2023-01-11 20:16:01 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     open: true,
-    port: 8000,
+    port: 3000,
   },
   plugins: [react()],
 })
