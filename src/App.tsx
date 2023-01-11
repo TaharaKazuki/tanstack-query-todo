479488de9ca3b4d52f58d571097897892b73ac57
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-01-11 20:34:17 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { FC, ReactElement } from 'react'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
+import { customTheme } from './theme/customTheme'
 
 const App: FC = (): ReactElement => {
-  return <div className="App">Hello world</div>
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <h1>Hello world</h1>
+    </ThemeProvider>
+  )
 }
 
 export default App
--- src/theme/customTheme.ts
+++ src/theme/customTheme.ts
@@ -1,5 +1,16 @@
 import { createTheme, ThemeOptions } from '@mui/material'
 
 export const customTheme: ThemeOptions = createTheme({
-  palette: {},
+  palette: {
+    mode: 'dark',
+    primary: {
+      light: 'rgba(168,85,247,.80)',
+      main: 'rgba(168,85,247,.65)',
+      dark: 'rgba(168,85,247,.28)',
+    },
+    background: {
+      paper: '#151515',
+      default: 'rgba(0,0,0,.96)',
+    },
+  },
 })
