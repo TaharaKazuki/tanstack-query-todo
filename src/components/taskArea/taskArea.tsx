e54701568136d85377a1332fe4f322850f42b609
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-01-17 00:16:49 +0900

    update

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -1,11 +1,13 @@
 import { FC, ReactElement } from 'react'
 import { Box, Grid } from '@mui/material'
+import { format } from 'date-fns'
 
 export const TaskArea: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
       <Box mb={8} px={4}>
-        <h2>Content Area</h2>
+        <h2>Status Of Your Tasks As On</h2>
+        {format(new Date(), 'PPPP')}
       </Box>
     </Grid>
   )
