5e7c44bc0d41dca22ceea9fc89b4e8069042d0bc
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-01-21 16:23:50 +0900

    update

--- src/components/taskCounter/taskCounter.tsx
+++ src/components/taskCounter/taskCounter.tsx
@@ -5,7 +5,16 @@ export const TaskCounter: FC = (): ReactElement => {
   return (
     <>
       <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-        <Avatar>
+        <Avatar
+          sx={{
+            backgroundColor: 'transparent',
+            border: '5px solid',
+            width: '96px',
+            height: '96px',
+            marginBottom: '16px',
+            borderColor: 'warning.light',
+          }}
+        >
           <Typography>10</Typography>
         </Avatar>
         <Typography>Subtitle</Typography>
